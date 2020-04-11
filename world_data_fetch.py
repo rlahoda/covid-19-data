331dd886d373c57ede070e33fa376639d953f76e
Rob Lahoda <rob@roblahoda.com>
2020-04-11 16:17:06 -0400

    Updated world data fetch to remove old data before downloading new

--- world_data_fetch.py
+++ world_data_fetch.py
@@ -1,5 +1,8 @@
 import wget
+import os
 
+os.remove("world-countries.csv")
+print("File Removed!")
 print("Beginning file download with wget module")
 
 url = "http://opendata.ecdc.europa.eu/covid19/casedistribution/csv"
