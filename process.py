034d7b2b4e3090b3484e40e1398758faba3a37bb
Rob Lahoda <rob@roblahoda.com>
2020-06-18 13:09:47 -0400

    updated world data

--- process.py
+++ process.py
@@ -10,17 +10,17 @@ from world_data import countries_parse
 def move_convert():
     print("Starting processing")
     states_parse()
-    counties_parse()
+    # counties_parse()
     with open("us-states.json", "r") as statesJSON:
         print("States JSON loaded")
         statesData = json.load(statesJSON)
         statesJSON.close()
         statesStr = json.dumps(statesData)
-        with open("us-counties.json", "r") as countiesJSON:
-            print("Counties JSON loaded")
-            countiesData = json.load(countiesJSON)
-            countiesJSON.close()
-            countyStr = json.dumps(countiesData)
+        # with open("us-counties.json", "r") as countiesJSON:
+        #     print("Counties JSON loaded")
+        #     countiesData = json.load(countiesJSON)
+        #     countiesJSON.close()
+        #     countyStr = json.dumps(countiesData)
         with open("world-countries.json", "r") as countriesJSON:
             print("Countries JSON loaded")
             countriesData = json.load(countriesJSON)
